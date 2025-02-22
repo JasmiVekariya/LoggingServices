2e9989ccb67e489eddf09801e1788c6774a6daea
Jasmi Vekariya <jasmivekariya1@gmail.com>
2025-02-22 15:26:33 -0500

    Added the header comment in LogClient.java file

---
+++ TestClient/LogClient.java
@@ -0,0 +1,8 @@
+/*
+FILE          : LogClient.java
+PROJECT       : Assignment - 03
+PROGRAMMER    : Jasmi Vekariya
+                Prachi Patel
+FIRST VERSION : February 22, 2025
+DESCRIPTION   : The following program contains the code for the client which is used to test the service. 
+*/

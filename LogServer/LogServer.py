8433f3976719abdd720ee9a79372c4fa3ae48795
Jasmi Vekariya <jasmivekariya1@gmail.com>
2025-02-22 16:29:43 -0500

    Added the header comment in LogServer.py file - An initial start to the project.

---
+++ LogServer/LogServer.py
@@ -0,0 +1,9 @@
+
+'''
+FILE          : LogServer.py
+PROJECT       : Assignment - 03
+PROGRAMMER    : Jasmi Vekariya
+                Prachi Patel
+FIRST VERSION : February 22, 2025
+DESCRIPTION   : The following program contains the code for the service which listen for logging messages.
+'''

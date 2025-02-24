b22fd351f8bacc8ecd9cd1f297a2b66299184fac
Jasmi Vekariya <jasmivekariya1@gmail.com>
2025-02-24 15:10:51 -0500

    Added the code for the connection to the server in client file.

--- TestClient/LogClient.java
+++ TestClient/LogClient.java
@@ -6,3 +6,30 @@ PROGRAMMER    : Jasmi Vekariya
 FIRST VERSION : February 22, 2025
 DESCRIPTION   : The following program contains the code for the client which is used to test the service. 
 */
+
+import java.io.*;  
+import java.net.*; 
+import java.util.Random;
+import java.util.Scanner;
+
+public class LogClient
+ {
+    public static void main(String[] args) 
+    {
+        if (args.length != 2) 
+        {
+            System.out.println("java LogClient <server_ip> <server_port>");
+            return; 
+        }
+
+        String SERVER_HOST = args[0];
+        int SERVER_PORT = Integer.parseInt(args[1]);
+        try (Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true); 
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+             Scanner scanner = new Scanner(System.in))
+              { 
+                System.out.println("Connected to the logging server.");
+              }
+    }
+ }
